db28ada57a1bcf98875864787381a27f92d65e42
Jonah Tan <47470981+jonahtanjz@users.noreply.github.com>
2022-11-06 12:24:01 +0800

    Add font detector for firefox browsers

--- basicFingerprinting.js
+++ basicFingerprinting.js
@@ -12,10 +12,23 @@ async function getAvailableFonts() {
 
   const availableFonts = new Set();
 
-  // fontsList variable is obtained from the file fonts.js
-  for (const font of fontsList.values()) {
-    if (document.fonts.check(`12px "${font}"`)) {
-      availableFonts.add(font);
+  if (userAgent.indexOf("Firefox") >= 0) {
+    // If Firefox browser is used, will need to use an alternate
+    // method to check for fonts (see fontDetector.js)
+    const detector = new Detector();
+
+    // fontsList variable is obtained from the file fonts.js
+    for (const font of fontsList.values()) {
+      if (detector.detect(font)) {
+        availableFonts.add(font);
+      }
+    }
+  } else {
+    // fontsList variable is obtained from the file fonts.js
+    for (const font of fontsList.values()) {
+      if (document.fonts.check(`12px "${font}"`)) {
+        availableFonts.add(font);
+      }
     }
   }
 
